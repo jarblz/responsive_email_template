535eed4d359ccc9d874c796fdb472e4b0c02c4ed
jared <jaredables@gmail.com>
2018-07-19 14:32:37 -0700

    initial commit

---
+++ lib/responsive_email_template.rb
@@ -0,0 +1,5 @@
+require "responsive_email_template/version"
+
+module ResponsiveEmailTemplate
+  # Your code goes here...
+end
---
+++ lib/responsive_email_template/version.rb
@@ -0,0 +1,3 @@
+module ResponsiveEmailTemplate
+  VERSION = "0.1.0"
+end
